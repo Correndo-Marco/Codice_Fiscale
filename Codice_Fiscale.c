db53300f85669f80e41df5504c3c5419b04fe1eb
Correndo-Marco <marco.correndo@gmail.com>
2025-03-04 00:28:28 +0100

    Creazione header con funzioni del codice fiscale

--- Codice_Fiscale.c
+++ Codice_Fiscale.c
@@ -1,39 +1,19 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
+#include "Funzioni_Codice_Fiscale.h"
 
 #define FILE_COM "comuni.txt"
 #define FILE_VAL "valori.txt"
-#define LENGHT_CF 16
+#define LENGTH_CF 16
 #define CURRENT_YEAR 2025
 #define MAX_LETTERS 50
 #define MAX_FILE 30
 
-char mesi[]="ABCDEHMLPRST";
-
-void printascelte();
-char da_int_a_char(int a);
-int is_date_valid(int *dd,int *mm,int *yyyy);
-int is_letter(char a);
-int is_vocal(char a);
-void get_nome(char *nome);
-void post_nome(char *codice,char *nome);
-void get_cognome(char *cognome);
-void post_cognome(char *codice,char *cognome);
-void get_data(int *dd,int *mm,int *yyyy);
-void post_date(char *codice,int *dd,int *mm,int *yyyy,char *genere);
-void get_genere(int *dd);
-int get_comune(char *cod_com);
-int post_comune(char *codice,char *comune);
-void calcola_lettere(char *elemento,char *consonanti,char *vocali,int *maxcons,int *maxvocali);
-void calcola_config(char *cons_cog,char *cons_nom,int *maxcons_cog,int *maxcons_nom,char *voc_cog,char *voc_nom,int *maxvoc_cog,int *maxvoc_nom,char *config);
-char get_special(char *codice);
-void crea_codice(char *codice,char *nome,char *cognome,int *dd,int *mm,int *yyyy,char *cod_com);
-void trasforma_diretta(char *codice);
-int trasforma_inversa(char *codice);
+const char mesi[]="ABCDEHMLPRST";
 
 int main(){
-    char codice[LENGHT_CF +1]; //spazio per il terminatore di stringa che potrebbe causare problemi
+    char codice[LENGTH_CF +1]; //spazio per il terminatore di stringa che potrebbe causare problemi
     printf("\n----\tCalcolatore di codice fiscale\t----\n\n");
     int sce;
     do{
@@ -49,7 +29,7 @@ int main(){
                 trasforma_inversa(codice);
                 break;
             default:
-                printf("Scelta non disponibile");
+                printf("Scelta non disponibile\n");
                 break;
         }
     }while(sce != 0);
@@ -292,7 +272,7 @@ char get_special(char *codice){
     fclose(fp);
 
     int somma= 0;
-    for(int i= 0; i< LENGHT_CF - 1; i++){
+    for(int i= 0; i< LENGTH_CF - 1; i++){
         somma += (i % 2 != 0) ? *(valori_pari + (int)*(codice + i)) : *(valori_dispari + (int)*(codice + i)) ;
                 // scambio pari e dispari 
     }
---
+++ Funzioni_Codice_Fiscale.h
@@ -0,0 +1,25 @@
+#ifndef FUNZIONI_CODICE_FISCALE_H
+#define FUNZIONI_CODICE_FISCALE_H
+
+void printascelte();
+char da_int_a_char(int a);
+int is_date_valid(int *dd,int *mm,int *yyyy);
+int is_letter(char a);
+int is_vocal(char a);
+void get_nome(char *nome);
+void post_nome(char *codice,char *nome);
+void get_cognome(char *cognome);
+void post_cognome(char *codice,char *cognome);
+void get_data(int *dd,int *mm,int *yyyy);
+void post_date(char *codice,int *dd,int *mm,int *yyyy,char *genere);
+void get_genere(int *dd);
+int get_comune(char *cod_com);
+int post_comune(char *codice,char *comune);
+void calcola_lettere(char *elemento,char *consonanti,char *vocali,int *maxcons,int *maxvocali);
+void calcola_config(char *cons_cog,char *cons_nom,int *maxcons_cog,int *maxcons_nom,char *voc_cog,char *voc_nom,int *maxvoc_cog,int *maxvoc_nom,char *config);
+char get_special(char *codice);
+void crea_codice(char *codice,char *nome,char *cognome,int *dd,int *mm,int *yyyy,char *cod_com);
+void trasforma_diretta(char *codice);
+int trasforma_inversa(char *codice);
+
+#endif
